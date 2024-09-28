d29508a668a69fffeffa6c2e6e87676f57c24167
sofkp <165909907+sofkp@users.noreply.github.com>
2024-09-27 20:27:25 -0500

    Update ingesta.py

--- ingesta.py
+++ ingesta.py
@@ -1,9 +1,27 @@
 import boto3
+import pandas as pd
+import mysql.connector
+
+def mysql_to_csv(host, port, user, password, database, table, ficheroUpload):
+    connection = mysql.connector.connect(
+        host=host,
+        port=port,
+        user=user,
+        password=password,
+        database=database
+    )
+
+    query = f"SELECT * FROM {table}"
+    df = pd.read_sql(query, connection)
+    df.to_csv(ficheroUpload, index=False)
+
+    connection.close()
 
 ficheroUpload = "data.csv"
-nombreBucket = "gcr-output-01"
+nombreBucket = "skp-output-ingesta02"
 
-s3 = boto3.client('s3')
-response = s3.upload_file(ficheroUpload, nombreBucket, ficheroUpload)
+mysql_to_csv('3.86.145.216', '8001', 'root', 'utec', 'tienda', 'fabricantes', ficheroUpload)
+
+res = boto3.client('s3').upload_file(ficheroUpload, nombreBucket, ficheroUpload)
 
 print("Ingesta completada")
